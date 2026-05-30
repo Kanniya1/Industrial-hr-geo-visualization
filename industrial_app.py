512dcd35a956536708fa23c6a86ecda0d07ea8f1
Kanniya1 <kanniyadevi182005s@gmail.com>
2026-05-30 09:01:01 -0700

    Update industrial_app.py

--- industrial_app.py
+++ industrial_app.py
@@ -104,7 +104,6 @@ state_workers=df.groupby("India/States")["Main Workers - Total -  Persons"].sum(
 fig=px.bar(state_workers,x="India/States",y="Main Workers - Total -  Persons")
 st.plotly_chart(fig)
 print(public_url)
-streamlit run industrial_app.py
 import os
 print(os.path.exists("industrial_app(3).py"))
 from google.colab import files
