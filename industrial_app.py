cbeab675d9f95134ea7bb7dcf0ad64fdb13ed6fc
Kanniya1 <kanniyadevi182005s@gmail.com>
2026-05-30 08:16:21 -0700

    Add files via upload

---
+++ industrial_app.py
@@ -0,0 +1,115 @@
+import glob
+files=glob.glob("/content/*.csv")
+print(files)
+!pip install pandas numpy plotly streamlit scikit-learn nltk
+import os
+for f in os.listdir('/content'):
+  print(f)
+import pandas as pd
+import glob
+files=glob.glob("/content/*.csv")
+frames=[]
+for file in files:
+  try:
+    frames.append(
+        pd.read_csv(
+            file,
+            encoding="latin1",
+            engine="python",
+            on_bad_lines="skip"
+        )
+    )
+    print("loaded:",file)
+  except Exception as e:
+    print("Error:",file)
+    print(e)
+df=pd.concat(frames,ignore_index=True)
+print(df.shape)
+df.head()
+df.drop_duplicates(inplace=True)
+df.fillna("Unknown",inplace=True)
+print(df.isnull().sum())
+print(df.info())
+print(df.describe())
+print(df.columns)
+def classify_industry(text):
+  text=str(text).lower()
+  if "retail" in text:
+    return "Retail"
+  elif "agriculture" in text:
+    return "Agriculture"
+  elif "poultry" in text:
+    return "Poultry"
+  elif "manufacturing" in text:
+    return "Manufacturing"
+  elif "construction" in text:
+    return "Contruction"
+  else:
+    return "others"
+df["Industry_Category"]=df["India/States"].apply(classify_industry)
+df.head()
+state_workers=df.groupby("India/States")["Main Workers - Total -  Persons"].sum().reset_index()
+print(state_workers.head())
+import plotly.express as px
+fig=px.bar(state_workers,x="India/States",y="Main Workers - Total -  Persons",title="state wise performance")
+fig.show()
+print(df.columns.tolist())
+import plotly.express as px
+male=df["Main Workers - Total - Males"].sum()
+female=df["Main Workers - Total - Females"].sum()
+Gender_data=pd.DataFrame({"Gender":["Male","Female"],"Workers":[male,female]})
+fig=px.pie(Gender_data ,names ="Gender",values="Workers",title="male vs female workers")
+fig.show()
+for col in df.columns:
+  print(col)
+  import os
+print("merged_dataset.csv"in
+os.listdir('/content'))
+files=glob.glob('/content/*.csv')
+print("number of files:",len(files))
+df_list=[]
+for filr in files:
+  try:
+    temp=pd.read_csv(file,encoding='latin1')
+    df_list.append(temp)
+  except Exception as e:
+    print("error in",file,":",e)
+df.to_csv('/content/merged_dataset.csv',index=False)
+print("merged file created successfully")
+print("shape:",df.shape)
+import streamlit as st
+male=df['Main Workers - Total - Males'].sum()
+female=df['Main Workers - Total - Females'].sum()
+Gender_data=pd.DataFrame({'Gender':['male','female'],'workers':[male,female]})
+fig2=px.pie(Gender_data,names='Gender',values='workers',title='male vs female workers')
+st.plotly_chart(fig2)
+fig2.show()
+import os
+print(os.listdir())
+!ls
+import os
+print(os.path.exists("industrial_app.py"))
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+st.title("Industrial Human Resource Geo Visualization")
+df=pd.read_csv("merged_dataset.csv")
+st.dataframe(df.head())
+state_workers=df.groupby("India/States")["Main Workers - Total -  Persons"].sum().reset_index()
+fig=px.bar(state_workers,x="India/States",y="Main Workers - Total -  Persons")
+st.plotly_chart(fig)
+!streamlit run industrial_app.py
+!pip install pyngrok -q
+from pyngrok import ngrok
+ngrok.set_auth_token("3ERg6TDXipuizh9KSwMtwAgTBTn_7kbxmFst8DytYBLqpyHZz")
+public_url=ngrok.connect(8501)
+print(public_url)
+!streamlit run industrial_app.py
+import os
+print(os.path.exists("industrial_app(3).py"))
+from google.colab import files
+files.download('industrial_app.py')
+import streamlit as st
+from google .colab import files
+files.download('merged_dataset.csv')
