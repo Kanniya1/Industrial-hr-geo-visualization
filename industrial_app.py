dc4c8be0b1fa55c3689853544ee0150bcacfd808
Kanniya1 <kanniyadevi182005s@gmail.com>
2026-05-30 08:57:06 -0700

    Update industrial_app.py

--- industrial_app.py
+++ industrial_app.py
@@ -103,7 +103,7 @@ st.dataframe(df.head())
 state_workers=df.groupby("India/States")["Main Workers - Total -  Persons"].sum().reset_index()
 fig=px.bar(state_workers,x="India/States",y="Main Workers - Total -  Persons")
 st.plotly_chart(fig)
-streamlit run industrial_app.py
+
 from pyngrok import ngrok
 ngrok.set_auth_token("3ERg6TDXipuizh9KSwMtwAgTBTn_7kbxmFst8DytYBLqpyHZz")
 public_url=ngrok.connect(8501)
