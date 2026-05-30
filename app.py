ccc3a348c001a928ff95a3327b83f3ae5091ad19
Kanniya1 <kanniyadevi182005s@gmail.com>
2026-05-30 00:58:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly .express as px
-df=pd.read_csv("DDW_B18sc_1600_NIC_FINAL_STATE_TRIPURA-2011.csv")
+df=pd.read_csv("DDW_B18sc_1600_NIC_FINAL_STATE_TRIPURA_2011.csv")
 st.title("Industrial humamn resource geo visualization")
 st.write(df.head())
 fig=px.bar(df,x="NIC Name",y="Main Workers - Total -  Persons")
